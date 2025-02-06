7c54cf0f2d1cd14a6b0a8b93308d1b30fde9edd0
mclarke16 <107970544+mrrogers16@users.noreply.github.com>
2025-02-06 16:18:34 -0600

    Added new transform logic for item box

---
+++ Assets/Scripts/ContainerCounter.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class ContainerCounter : MonoBehaviour, IKitchenObjectParent
+{
+
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+    private KitchenObject kitchenObject;
+
+    public void Interact(Player player)
+    {
+        // If nothings there
+        if (kitchenObject == null)
+        {   // Create kitchen object using prefab at spec'ed point
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
+        }
+        else
+        {
+            // Give the object to the player
+            kitchenObject.SetKitchenObjectParent(player);
+        }
+    }
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
+    }
+}
